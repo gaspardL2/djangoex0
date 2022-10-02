5b8e8fed415527e2078539349a8f69ff5587b29a
gaspardL2 <gaspard.tourk@tutanota.com>
2022-10-02 19:50:43 +0200

    changed views.py

--- nations/views.py
+++ nations/views.py
@@ -17,7 +17,7 @@ class CountryFormView(FormView):
 
 class FormSuccessView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Search done")
+        return HttpResponse("Search finished")
 
 
 
